b7cd70b36ed456551ee433a285cb0b53710467d3
Nick <nicholasee97@gmail.com>
2022-10-20 00:56:48 +0800

    Learn repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
 
+const repeatString = function(para1, para2) {
+    if (para2 < 0) {
+        return "ERROR";
+    }
+    for ( let i = 0; i < 3; i++) {
+        return para1.repeat(para2);
+        }
+    
+    
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+
 
 // Do not edit below this line
 module.exports = repeatString;
