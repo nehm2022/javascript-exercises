521171cc47566464ca27e91b64e2eddae33ed9db
Nick <nicholasee97@gmail.com>
2022-10-20 22:59:26 +0800

    Passed reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = "";
+    for ( i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 };
+
+reverseString('hello');
+reverseString('hello there')
+reverseString('123! abc!')
+reverseString('')
+
 
 // Do not edit below this line
 module.exports = reverseString;
