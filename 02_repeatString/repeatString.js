d6010a449fa8afe8db4c6fa7f377632c1a1503c1
Nick <nicholasee97@gmail.com>
2022-10-22 12:35:45 +0800

    Solutions incorrect

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,9 @@
 
-const repeatString = function(para1, para2) {
+/*const repeatString = function(para1, para2) {
     if (para2 < 0) {
         return "ERROR";
     }
-    for ( let i = 0; i < 3; i++) {
+    for ( let i = 0; i < para2; i++) {
         return para1.repeat(para2);
         }
     
@@ -15,6 +15,19 @@ repeatString('hey', 10);
 repeatString('hey', 1);
 repeatString('hey', 0);
 repeatString('hey', -1);
+*/
+
+const repeatString = function(word, times) {
+    if (times < 0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+
+
 
 
 // Do not edit below this line
